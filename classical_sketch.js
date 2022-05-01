5df86453494d0c09d5a2757e65009f37185ae8ff
Stephen Fay <dcxstephen@live.co.uk>
2022-05-01 13:08:44 +0100

    cleanup

--- classical_sketch.js
+++ classical_sketch.js
@@ -1,9 +1,7 @@
-// let state = []; // the phase space state x1,y1,vx1,vy1,x2,y2,vy2,etc.
-// let masses = []; // the masses, must have len = len state / 4
-let h = 0.001; // the time step 
-let G = 10000;
-let canvx = 400;
-let canvy = 400;
+const h = 0.001; // the time step constant
+const G = 10000;
+const canvx = 400;
+const canvy = 400;
 
 function setup() {
   frameRate(60);
@@ -12,22 +10,24 @@ function setup() {
   state = [];
   state.push(new Mass(0,100,900,0,10));
   state.push(new Mass(0,-100,-900,0,12));
+  state.push(new Mass(100,0,0,-900,20));
+  state.push(new Mass(-100,0,0,900,20));
   state.push(new Mass(0,0,0,0,10000));
 }
 
 function draw() {
-  time += h; 
+  time += h;
   background(0);
-  
+
   // update
-  // update_explicit_euler(state);
-  update_rk4(state);
-  
+  // update_explicit_euler(state); // update using explicit euler method
+  update_rk4(state); // update the state using 4th order runge-kutta method
+
   // draw
   state.forEach((mass) => {
     mass.display();
   })
-  
+
 }
 
 // takes state: array of Mass objects and updates them by timestep
@@ -45,11 +45,10 @@ function update_explicit_euler(state) {
   }
 }
 
+// helpers for update_rk4
 function divby2(num) {return num/2}
 function divby3(num) {return num/3}
-// function multby2(num) {return num*2}
 function divby6(num) {return num/6}
-
 
 // takes state: array of Mass objects and updates them by a timestep
 function update_rk4(state) {
@@ -57,18 +56,18 @@ function update_rk4(state) {
   // k1 = f(y_n) // not time-dependent
   // k2 = f(y_n + h*k1 / 2)
   // k3 = f(y_n + h*k2 / 2)
-  // k4 = f(y_n + h*k3) 
-  
+  // k4 = f(y_n + h*k3)
+
   let k1 = []; // same length as state
   let k2 = []; // same length as state
   let k3 = []; // same length as state
   let k4 = []; // same length as state
-  
+
   // Calculate k1
   state.forEach((mass, idx) => {
     k1.push(mass.get_gradient(state,idx));
   });
-  
+
   // Calculate k2
   let state_copy = deep_copy_state(state);
   // update the state copy
@@ -79,7 +78,7 @@ function update_rk4(state) {
   state_copy.forEach((mass, idx) => {
     k2.push(mass.get_gradient(state_copy, idx));
   });
-  
+
   // Calculate k3
   state_copy = deep_copy_state(state);
   // update the state copy
@@ -90,7 +89,7 @@ function update_rk4(state) {
   state_copy.forEach((mass, idx) => {
     k3.push(mass.get_gradient(state_copy, idx));
   });
-  
+
   // Calculate k4
   state_copy = deep_copy_state(state);
   // update the state copy
@@ -101,9 +100,9 @@ function update_rk4(state) {
   state_copy.forEach((mass, idx) => {
     k4.push(mass.get_gradient(state_copy, idx));
   });
-  
-  // update the actual state 
-  // dy_n+1 = y_n + 1/6 h(k1 + k2 + k3 + k4) 
+
+  // update the actual state
+  // dy_n+1 = y_n + 1/6 h(k1 + k2 + k3 + k4)
   for (let idx = 0; idx < state.length; idx ++) {
     let mass = state[idx]; // this is a Mass object
     mass.add_vec(k1[idx].map(divby6));
@@ -123,11 +122,11 @@ function deep_copy_mass(mass_obj) {
   return mass_obj.deep_copy();
 }
 
-// This computes the acceleration due to potential Hamiltonian 
+// This computes the acceleration due to potential Hamiltonian
 function get_accel_from(x1,y1,m1,x2,y2,m2) {
   r21 = [x1-x2,y1-y2]; // r pointing from 2 to 1
   rabs = Math.sqrt(r21[0]*r21[0] + r21[1]*r21[1]);
-  ax = -G*m2/(rabs*rabs*rabs) * r21[0];
+  ax = -G*m2/(rabs*rabs*rabs) * r21[0]; // m1 not used
   ay = -G*m2/(rabs*rabs*rabs) * r21[1];
   return [ax,ay];
 }
@@ -140,7 +139,7 @@ class Mass {
     this.vy = vy;
     this.m = m;
   }
-  
+
   // Explicit Euler; doesn't modify state, just returns value
   get_gradient (state,idx) {
     let ax = 0; // acceleration at this time
@@ -155,23 +154,21 @@ class Mass {
     });
     // correct formalism dy/dt = f(y)
     let this_f_of_y = [this.vx, this.vy, ax, ay]; // unused, just for reader
-    return this_f_of_y; // [dx,dy,dvx,dvy]
-    // [this.x,this.y] = [this.x + h*this.vx,this.y + h*this.vy];
-    // [this.vx,this.vy] = [this.vx + h*ax,this.vy + h*ay];
+    return this_f_of_y; // [dx/dt,dy/dt,dvx/dt,dvy/dt]
   }
-  
+
   add_vec (foy) {
     let [xprime,yprime,vxprime,vyprime] = foy;
-    this.x += h * xprime;
+    this.x += h * xprime; // h is the global time-step constant defined top
     this.y += h * yprime;
     this.vx += h * vxprime;
     this.vy += h * vyprime;
   }
-  
+
   deep_copy () {
     return new Mass(this.x,this.y,this.vx,this.vy,this.m);
   }
-  
+
   display (idx) {
     fill(255 - 40*idx);
     circle(this.x + canvx/2,this.y + canvy/2,min(2*Math.cbrt(this.m),10));
